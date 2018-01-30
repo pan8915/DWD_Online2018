3842a76d42af9c2ab1037b80809d58a0b35c72cd
Pan Pan <panpan@172-16-222-247.DYNAPOOL.NYU.EDU>
2018-01-30 14:54:14 -0500

    change port

--- Week_2/server.js
+++ Week_2/server.js
@@ -5,9 +5,21 @@ app.use(express.static('public'));
 
 var count =0;
 
+var thesubmissions =[];
 app.get('/formpost', function (req,res){
     res.send("you submitted"+ req.query.textfild);
+    thesubmissions.push(req.query.textfild);
 });
+
+app.get('/display', function(req,res){
+    var htmlout = "<html><body>";
+    for ( var i = 0; i <thesubmissions.length; i++){
+        htmlout = htmlout + thesubmissions[i] +"<br>";
+    }
+    var htmlout =htmlout + "<html><body>";
+    res.send(htmlout);
+});
+
 
 app.get('/count', function(req,res){
     count++;
@@ -21,6 +33,6 @@ app.get('/get something', function (req, res) {
     res.send('goodbye')
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!')
 });
