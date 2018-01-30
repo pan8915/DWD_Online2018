2bff69efd137faea37250573e2a5ea17cc4b55fc
Pan Pan <panpan@172-16-222-247.DYNAPOOL.NYU.EDU>
2018-01-30 14:28:47 -0500

    new html

--- Week_2/server.js
+++ Week_2/server.js
@@ -3,6 +3,17 @@ var app = express();
 
 app.use(express.static('public'));
 
+var count =0;
+
+app.get('/formpost', function (req,res){
+    res.send("you submitted"+ req.query.textfild);
+});
+
+app.get('/count', function(req,res){
+    count++;
+    //res.send
+});
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
