709149855659846071af6a2a9238e025686fdb35
Pan Pan <panpan@172-16-222-247.DYNAPOOL.NYU.EDU>
2018-01-30 14:07:06 -0500

    new file add

--- Week_2/server.js
+++ Week_2/server.js
@@ -1,10 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
+app.get('/get something', function (req, res) {
+    res.send('goodbye')
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-})
+});
