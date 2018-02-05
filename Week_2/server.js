8e8f8abaeb265ebf328a1678cc66df294faaabbe
Pan Pan <panpan@Pans-MacBook-Pro.local>
2018-02-05 12:48:10 -0500

    package.json

--- Week_2/server.js
+++ Week_2/server.js
@@ -6,33 +6,35 @@ app.use(express.static('public'));
 var count =0;
 
 var thesubmissions =[];
+
 app.get('/formpost', function (req,res){
-    res.send("you submitted"+ req.query.textfild);
-    thesubmissions.push(req.query.textfild);
-});
+    //res.send("you submitted"+ req.query.textfild);
+    thesubmissions.push(req.query.textfield);
+    res.redirect('/display');
+})
 
 app.get('/display', function(req,res){
     var htmlout = "<html><body>";
-    for ( var i = 0; i <thesubmissions.length; i++){
-        htmlout = htmlout + thesubmissions[i] +"<br>";
+    for ( var i = 0; i <thesubmissions.length; i++) {
+        htmlout = htmlout + thesubmissions[i] + "<br>";
     }
-    var htmlout =htmlout + "<html><body>";
+    htmlout =htmlout + "</body></html>";
     res.send(htmlout);
-});
+})
 
 
 app.get('/count', function(req,res){
     count++;
-    //res.send
-});
+    res.send('<html><body><h1>'+ count + '</h1></body></html>');
+})
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
-app.get('/get something', function (req, res) {
-    res.send('goodbye')
-});
+    res.send('Hello World!');
+})
+app.get('/somethingelse', function (req, res) {
+    res.send('Goodbye');
+})
 
-app.listen(80, function () {
-    console.log('Example app listening on port 3000!')
-});
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000')
+})
